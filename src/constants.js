c1a6daaa41884ef8e904b2cce3d8b619a28e97cd
Patrick Porto <patrick.s.porto@gmail.com>
2023-11-13 00:37:26 -0700

    chore: setup project

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const CANONICAL_NAME = 'fvtt-module-template';
-export const MODULE_NAME = 'FVTT Module Template';
+export const CANONICAL_NAME = 'audiopids-lib';
+export const MODULE_NAME = 'Audio Pids Lib';
 export const MODULE_PATH = `modules/${CANONICAL_NAME}`;
 export const TEMPLATE_PATH = `${MODULE_PATH}/templates`;
